7f2510ab331472a7fc9b1d3943e8abb1311c093c
SanjeevaPereddy <133981576+SanjeevaPereddy@users.noreply.github.com>
2023-05-19 01:45:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,9 @@
 import streamlit
 
-streamlit.title('My parents New Healthy Diner')
 streamlit.title('My Mom's New Healthy Diner')
 
 streamlit.header('Breakfast Favourites')
 streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗Kale, Spinach and Rocket Smoothie')
-streamlit.text('🐓Hard-Boiled Free-Range egg')
+streamlit.text('🐔Hard-Boiled Free-Range egg')
 streamlit.text('🥑🍞Avocado Toast')
